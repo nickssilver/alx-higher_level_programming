62f81b3dd58ca14b4cf98ce68452a7b2247b2770
nickssilver <51230602+nickssilver@users.noreply.github.com>
2022-09-20 21:57:00 +0300

    insert into a sorted linked list, preserving the sort:fire:

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,20 +1,18 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-#include <stddef.h>
-
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * for Holberton project
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -24,5 +22,3 @@ void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
-Footer
-© 2022 GitHub, Inc.
