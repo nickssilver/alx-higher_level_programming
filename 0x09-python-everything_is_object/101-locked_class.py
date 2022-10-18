82a5ffbf7d307921a61776942b8fd3fc7a922293
nickssilver <51230602+nickssilver@users.noreply.github.com>
2022-10-19 01:19:33 +0300

    Error corrected:bulb:

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-class LockedClass:
+""" LockedClass class """
 
-    __slots__ = ['first_name']
 
-    def __init__(self, first_name=''):
-        self.first_name = first_name
+class LockedClass():
+    """ variables declarations """
+    __slots__ = ('first_name')
